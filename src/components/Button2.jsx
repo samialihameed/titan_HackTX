05c070bbeb9de132a7d5de7197aaa2164e535ca8
Don Dang <dondang@Dons-MacBook-Pro.local>
2023-10-22 12:18:24 -0500

    updated link

--- src/components/Button2.jsx
+++ src/components/Button2.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Button2.css";
 
 const Button2 = ({ styles }) => (
-  <a href="https://tinyurl.com/employeesAI" target="_blank" rel="noopener noreferrer">
+  <a href="https://employease-ai.azurewebsites.net/" target="_blank" rel="noopener noreferrer">
     <button
       type="button"
       className={`button-glow py-4 px-6 font-poppins font-medium text-[18px] text-white bg-blue-gradient rounded-[10px] outline-none ${styles} `}
